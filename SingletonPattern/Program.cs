c4cfab5ee68d3653fb1d4020d5b3fd03f1d41996
shubhanshu.rastogi <shubhanshu.rastogi@klingelnberg.com>
2019-03-27 23:52:40 +0530

    Singleton Pattern implemented

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var sealedobject = SingletonPattern.SingletonObject;
+            sealedobject.Message = "Hi this is shubhanshu";
+            Console.WriteLine(sealedobject.Message);
+            var sealedobject2 = SingletonPattern.SingletonObject;
+            sealedobject2.Message = "Hi I tried to create another object";
+            Console.WriteLine(sealedobject.Message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class SingletonPattern
+    {
+        private static SingletonPattern _singletonObject;
+        public static SingletonPattern SingletonObject
+        {
+            get
+            {
+                if (_singletonObject != null) return _singletonObject;
+                _singletonObject = new SingletonPattern();
+                return _singletonObject;
+
+            }
+        }
+
+        private SingletonPattern(){}
+
+        public string Message { get; set; }
+    }
+    
+}
